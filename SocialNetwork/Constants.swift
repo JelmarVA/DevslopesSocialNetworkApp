7f4e267ac763ef1c5021cc56e35b81e8b0560a73
JelmarVA <jelmarvanaert@jelmars-air.home>
2017-02-22 17:56:17 +0100

    Sign in ui Custom calsses complete

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Jelmar Van Aert on 22/02/2017.
+//  Copyright © 2017 Jelmar Van Aert. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120/255
